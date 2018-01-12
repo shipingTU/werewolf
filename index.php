ea3c48ebf09e3533f7627383d5d63718399a0b82
Shiping <shiping.tu@davidson.fr>
2018-01-12 17:35:21 +0100

    change to chinese characters

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Werewolf Game</title>
+    <title>狼人杀</title>
 
     <!--link rel="icon" href="./source/favicon.ico"-->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -14,32 +14,32 @@
 
   <body>
     <div class="container">
-      <h2 class="form-signin-heading">The Werewolves of Millers Hollow</h2>	
-      <div class="form-group">
-        <a href="createGame.php" class="btn btn-primary btn-lg btn-block">Create a game</a>
+      <h2 class="form-signin-heading" align='center'>狼人杀</h2>	
+      <div class="form-group" align='center'>
+        <a href="createGame.php" class="btn btn-primary btn-lg">创建游戏</a>
       </div>
+      <div class="form-group" align='center'>
         <form id="enterGameForm" action="game.php" method="post">
-          <div class="form-group">
-            <select id="positions" name="positions" class="form-control form-control-xs">
-              <?php
-                include 'func/base.php';
-                $bd=connectSql();
-                $sql="SELECT pos from roles where used=1 and entred!=1 order by pos asc";
-                $vQuery=mysqli_query($bd, $sql);
-                if ( $vQuery ) {
-                  echo '<option selected></option>'; 
-                  while($row = mysqli_fetch_array($vQuery, MYSQLI_ASSOC)) {
-                      echo '<option>'.$row['pos'].'</option>';
-                  }       
-                }
-                mysqli_close($bd);
-                ?>
-            </select>
-          </div>
-          <div class="form-group">
-            <button id="enterGame" type="button" class="btn btn-primary btn-lg btn-block">Enter the game</button>
-          </div>
-        </form>
+          <select id="positions" name="positions" class="form-control-xs">
+            <?php
+              include 'func/base.php';
+              $bd=connectSql();
+              $sql="SELECT pos from roles where used=1 and entred!=1 order by pos asc";
+              $vQuery=mysqli_query($bd, $sql);
+              if ( $vQuery ) {
+                echo '<option selected>位置</option>'; 
+                while($row = mysqli_fetch_array($vQuery, MYSQLI_ASSOC)) {
+                    echo '<option>'.$row['pos'].'</option>';
+                }       
+              }
+              mysqli_close($bd);
+              ?>
+          </select>
+        </form>  
+      </div>
+      <div class="form-group" align='center'>
+        <button id="enterGame" type="button" class="btn btn-primary btn-lg">进入游戏</button>
+      </div>
     </div>
   </body>
 </html>
